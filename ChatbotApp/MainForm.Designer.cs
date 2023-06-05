652a5e959044ecbbba02442b078c8205c84fe1a6
sanokatana <chandrazahran@gmail.com>
2023-06-05 17:55:30 +0700

    Add project files.

---
+++ ChatbotApp/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ChatbotApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBotOutput = new System.Windows.Forms.TextBox();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBotOutput
+            // 
+            this.txtBotOutput.Location = new System.Drawing.Point(12, 12);
+            this.txtBotOutput.Multiline = true;
+            this.txtBotOutput.Name = "txtBotOutput";
+            this.txtBotOutput.Size = new System.Drawing.Size(304, 310);
+            this.txtBotOutput.TabIndex = 0;
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(12, 340);
+            this.txtUserInput.Multiline = true;
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(234, 66);
+            this.txtUserInput.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(252, 340);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(64, 66);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 432);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.txtBotOutput);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBotOutput;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatbotApp/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatbotApp
+{
+    public partial class MainForm : Form
+    {
+        private const string ApiKey = "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";
+        private const string ApiUrl = "https://api.openai.com/v1/chat/completions";
+
+        private readonly HttpClient client;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ApiKey}");  
+        }
+        private async Task<string> GetChatGPTReply(string userMessage)
+        {
+            var requestBody = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+            new { role = "user", content = userMessage }
+        }
+            };
+            var requestBodyJson = Newtonsoft.Json.JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(requestBodyJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(ApiUrl, content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            dynamic responseData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseContent);
+
+            if (responseData != null && responseData.choices != null && responseData.choices.Count > 0)
+            {
+                var chatGptReply = responseData.choices[0]?.message?.content?.ToString();
+                if (!string.IsNullOrEmpty(chatGptReply))
+                {
+                    return chatGptReply;
+                }
+            }
+
+            throw new InvalidOperationException($"Invalid response from ChatGPT API. Response: {responseContent}");
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            var userMessage = txtUserInput.Text.Trim();
+            if (!string.IsNullOrEmpty(userMessage))
+            {
+                txtBotOutput.AppendText("You: " + userMessage + Environment.NewLine);
+                txtUserInput.Clear();
+
+                var chatGptReply = await GetChatGPTReply(userMessage);
+                txtBotOutput.AppendText("ChatGPT: " + chatGptReply + Environment.NewLine);
+            }
+        }
+    }
+}
